bd37b82258ab65bf6a162154eeaaf89f15b3b530
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2021-12-09 16:30:52 +0900

    2021-12-09 이전 커밋 수업 15일차 복습 내용임

--- kakao_web_kim/src/main/java/com/kakao/web/notice/model/dao/NoticeDao.java
+++ kakao_web_kim/src/main/java/com/kakao/web/notice/model/dao/NoticeDao.java
@@ -3,7 +3,7 @@ package com.kakao.web.notice.model.dao;
 import java.util.List;
 
 import com.kakao.web.notice.model.dto.NoticeDto;
-
+//
 public interface NoticeDao {
 
 	public List<NoticeDto> getNoticeAll();
